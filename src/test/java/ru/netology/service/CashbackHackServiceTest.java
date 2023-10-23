aac99765052921d5a29f3cef859fa89a50b06471
Oksana <oksana017m@mail.com>
2023-10-23 16:12:48 +0300

    add test with defect

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -30,4 +30,13 @@ public class CashbackHackServiceTest {
         int expected = 999;
         assertEquals(expected, actual);
     }
+
+    @org.junit.Test
+    public void TestIfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
