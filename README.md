9b1d90ac57a61818576f7220df6998e4a69470e5
Oksana <oksana017m@mail.com>
2023-10-23 16:01:54 +0300

    add test with defect

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+[![Java CI with Gradle](https://github.com/Oksana017/CashBackHack/actions/workflows/gradle.yml/badge.svg)](https://github.com/Oksana017/CashBackHack/actions/workflows/gradle.yml)
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -31,4 +31,13 @@ public class CashbackHackServiceTest {
         int expected = 999;
         assertEquals(actual, expected);
     }
+
+    @org.testng.annotations.Test
+    public void TestIfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
